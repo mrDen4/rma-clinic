ec0c2c74260a4b3cebbdbe0704c5b2f6dcb82654
Dennis <mr.den.saint@yandex.ru>
2023-01-13 01:13:59 +0400

    add layout

---
+++ dist/assets/b03bee21659880163986.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination'
+  }
+});
---
+++ src/scripts/main.js
@@ -0,0 +1,3 @@
+const a = 3 + 5;
+
+console.log(a);
